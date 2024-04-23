93284581bb2c25876ef9a5092626357b5458286f
Henry Cortez <100394133+HenryCortez@users.noreply.github.com>
2024-04-23 08:35:46 -0500

    Add files via upload

---
+++ larabel/resources/views/estudiante/buscador.blade.php
@@ -0,0 +1,43 @@
+@extends('estudiante/template')
+@section('title', 'Mostrar Estudiante')
+@section('content')
+
+<form action="{{ route('estudiantes.show') }}" method="GET">
+    <input type="text" name="id" placeholder="Buscar estudiante">
+    <button type="submit">Buscar</button>
+</form>
+<h1 class="mb-4">Mostrar Estudiante</h1>
+    <table class="table table-striped">
+        <thead class="thead-dark">
+        <tr>
+            <th scope="col">Cedula</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Apellido</th>
+            <th scope="col">Direccion</th>
+            <th scope="col">Telefono</th>
+        </tr>
+        </thead>
+        <tbody>
+            @foreach ($estudianteArray as $estudiante)
+                <tr>
+                    <td>{{$estudiante['id']}}</td>
+                    <td>{{$estudiante['nombre']}}</td>
+                    <td>{{$estudiante['apellido']}}</td>
+                    <td>{{$estudiante['direccion']}}</td>
+                    <td>{{$estudiante['telefono']}}</td>
+                 
+                    <td><a href="{{ url('/estudiantes/' . $estudiante['id']) . '/edit' }}" class="btn btn-primary">Editar</a></td>
+                  
+                    <td>
+                <form action="{{ url('/estudiantes/'.$estudiante['id']) }}" method="post">
+                    @method('DELETE')
+                    @csrf
+                    <button type="submit" class="btn btn-danger">Eliminar</button>
+                    </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+<a href="{{ url('/estudiantes/create') }}" class="btn btn-success">Nuevo</a>
+@endsection
